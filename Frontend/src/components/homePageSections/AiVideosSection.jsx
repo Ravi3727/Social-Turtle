d318561fe068a559e7f1251272994fa690cc5c3f
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-09-04 22:24:42 +0530

    animation update

--- Frontend/src/components/homePageSections/AiVideosSection.jsx
+++ Frontend/src/components/homePageSections/AiVideosSection.jsx
@@ -26,7 +26,7 @@ function getCurveStyle(i, mid) {
         <div style={{
                   fontFamily: "Montserrat, sans-serif",
                   
-                }} className='min-h-screen flex flex-col gap-6 h-fit w-screen py-10 overflow-clip'>
+                }} className=' flex flex-col gap-6 h-fit w-screen py-10 overflow-clip'>
             <div className='flex flex-col w-screen  font-bold text-[28px] md:text-[56px] items-center justify-center leading-tight  text-center'>
                 <div>Engage Audience with Stunning</div>
                 <div style={{
@@ -39,45 +39,10 @@ function getCurveStyle(i, mid) {
                 </div>
            
 
-     {/* <motion.div className='flex relative justify-between items-center gap-6 '  // move left continuously
-          transition={{
-            duration: 10,
-            repeat: Infinity,
-            ease: "linear",
-          }}>
-        <div ref={cenRef} className='bg-red-400  w-10 h-5 absolute left-1/2 -translate-x-1/2' ></div>
-        {images.map((src, i) => {
-        const { scale } = getCurveStyle(i, Math.floor(images.length / 2));
-        return (
-          <motion.img
-            key={i}
-            src={src}
-            alt=""
-            id={`img-${i}`}
-            className={`w-[250px] h-[500px] flex-shrink-0 rotate-z-${(i - Math.floor(images.length / 2)) * 15}`}
-            style={{ scaleY: scale,  transformOrigin: 'center bottom' }} // Anchor scaling from bottom
-          />
-        );
-      })}
-      {images.map((src, i) => {
-        const { scale} = getCurveStyle(i, Math.floor(images.length / 2));
-        return (
-          <motion.img
-            key={i}
-            src={src}
-            alt=""
-            id={`img-${i}`}
-            className="w-[250px] h-[500px] flex-shrink-0"
-            style={{ scaleY: scale,  transformOrigin: 'center bottom' }} // Anchor scaling from bottom
-          />
-        );
-      })}
     
-
-     </motion.div> */}
-     <div>
+   
 <CurvedCarousel images={images} />
-     </div>
+ 
   
         </div>
     )
--- Frontend/src/components/homePageSections/CaseStudies.jsx
+++ Frontend/src/components/homePageSections/CaseStudies.jsx
@@ -16,10 +16,10 @@ const CaseStudies = () => {
  const [visible, setVisible] = useState(true);
   const timeoutRef = useRef(null);
 
-  const img1Y = useTransform(sc.scrollYProgress, [0, 0.25], ["150%", "0%"]);
-  const img2Y = useTransform(sc.scrollYProgress, [0.25, 0.5],["150%", "0%"]);
-  const img3Y = useTransform(sc.scrollYProgress, [0.5, 0.75],["150%", "0%"]);
-  const img4Y = useTransform(sc.scrollYProgress, [0.75, 1], ["150%", "0%"]);
+  const img1Y = useTransform(sc.scrollYProgress, [0, 0.15], ["150%", "0%"]);
+  const img2Y = useTransform(sc.scrollYProgress, [0.15, 0.30],["150%", "0%"]);
+  const img3Y = useTransform(sc.scrollYProgress, [0.30, 0.45],["150%", "0%"]);
+  const img4Y = useTransform(sc.scrollYProgress, [0.45, 0.60], ["150%", "0%"]);
 
 
 useEffect(() => {
@@ -54,20 +54,44 @@ useEffect(() => {
   };
 }, [cursorX, cursorY]);
 
-  const scale = useTransform(scrollYProgress, [0, 1], [1.1, 0.7]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1.1, 0.9]);
 
   return (
-    <div ref={ref}  className='bg-[#F6F6F6] w-screen min-h-[70vh] relative pt-10 pb-10  flex  gap-6 flex-col items-center'>
+    
+    <div ref={ref}  className='bg-[#F6F6F6] w-screen h-[80vh] md:h-[90vh] relative overflow-clip  pt-10 pb-10  flex  gap-6 flex-col items-center'>
         <div style={{
               fontFamily: "Calisga, serif",
            
             }} className='font-bold text-[28px] md:text-[49px]  text-center'>Case Studies</div>
-   <motion.div ref={animRef} className="w-full sticky top-0 flex flex-col items-center h-[570px] overflow-clip">
-  <motion.img style={{ scale, translateY: img1Y }} transition={{duration:1,type:"easeIn",delay:0.2}} src="/images/mountain.png" className="w-[359px] md:w-[95%] md:h-[554px] h-[400px] rounded-2xl mt-6 object-cover" alt="" />
-  <motion.img style={{ scale, translateY: img2Y }} transition={{duration:1,type:"easeIn",delay:0.2}} src="/images/mountain.png" className="w-[359px] md:w-[95%] absolute md:h-[554px] h-[400px] rounded-2xl mt-6 object-cover" alt="" />
-  <motion.img style={{ scale, translateY: img3Y }} transition={{duration:1,type:"easeIn",delay:0.2}} src="/images/mountain.png" className="w-[359px] md:w-[95%] absolute md:h-[554px] h-[400px] rounded-2xl mt-6 object-cover" alt="" />
-  <motion.img style={{ scale, translateY: img4Y }} transition={{duration:1,type:"easeIn",delay:0.2}} src="/images/mountain.png" className="w-[359px] md:w-[95%] absolute md:h-[554px] h-[400px] rounded-2xl mt-6 object-cover" alt="" />
-</motion.div>
+
+
+ <div className='w-full h-[120vh] relative '>
+        {/* Sticky pinned images */}
+        <div ref={animRef} className='sticky top-5 h-screen flex flex-col items-center  overflow-hidden'>
+          <motion.img
+            style={{ scale, y: img1Y }}
+            src="/images/mountain.png"
+            className="w-[359px] md:w-[95%] md:h-[554px] h-[400px] rounded-2xl mt-6 object-cover"
+          />
+          <motion.img
+            style={{ scale, y: img2Y }}
+            src="/images/mountain.png"
+            className="w-[359px] md:w-[95%] md:h-[554px] h-[400px] rounded-2xl absolute mt-6 object-cover"
+          />
+          <motion.img
+            style={{ scale, y: img3Y }}
+            src="/images/mountain.png"
+            className="w-[359px] md:w-[95%] md:h-[554px] h-[400px] rounded-2xl absolute mt-6 object-cover"
+          />
+          <motion.img
+            style={{ scale, y: img4Y }}
+            src="/images/mountain.png"
+            className="w-[359px] md:w-[95%] md:h-[554px] h-[400px] rounded-2xl absolute mt-6 object-cover"
+          />
+        </div>
+      </div>
+
+
       <motion.div
   style={{ x: cursorX, y: cursorY, opacity: visible ? 1 : 0, }}
   transition={{ type: "spring", stiffness: 50, damping: 10 }}
@@ -76,12 +100,14 @@ useEffect(() => {
  
 </motion.div>
       <motion.div
+      
   style={{ x: cursorX, y: cursorY, opacity: visible ? 1 : 0, }}
   transition={{ type: "spring", stiffness: 200, damping: 30 }}
   className="border-4 border-[#A0CB3A] h-2 w-2 rounded-full flex items-center justify-center absolute top-0 left-0 z-30 pointer-events-none -translate-x-1/2 -translate-y-1/2"
 >
  
 </motion.div>
+
  </div>
   )
 }
