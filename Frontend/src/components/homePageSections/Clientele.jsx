220d779918a51f3e869227e02430002925c5b2c4
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-09-11 18:30:29 +0530

    homepage responsiveness upgrade

--- Frontend/src/components/homePageSections/Clientele.jsx
+++ Frontend/src/components/homePageSections/Clientele.jsx
@@ -3,14 +3,14 @@ import { motion } from "framer-motion"
 
 const Clientele = () => {
   return (
-    <div className='w-screen h-[70vh]   flex flex-col justify-between items-center   '>
+    <div className='w-screen h-fit   flex flex-col justify-between items-center overflow-x-clip   '>
       <div  style={{
               fontFamily: "Calisga, serif",
            
             }}className=' text-[28px] md:text-[49px] text-center mt-10 text-black font-extrabold '>Clientele</div>
-      <div className='min-w-full relative flex flex-col justify-center items-center h-[90%]  overflow-clip'>
+      <div className='w-full relative h-fit flex flex-shrink-0 flex-col justify-center items-center  overflow-clip'>
         <motion.div
-          className="flex"
+          className="flex  justify-evenly items-center w-full  "
           animate={{ x: ["0%", "-100%", "0%"] }} // move left continuously
           transition={{
             duration: 20,
@@ -18,27 +18,53 @@ const Clientele = () => {
             ease: "linear",
           }}
         >
-          {/* Duplicate images to loop seamlessly */}
-          <img src="/images/row1.png" alt="" className="w-auto h-40" />
-          <img src="/images/row1.png" alt="" className="w-auto h-40" />
-          <img src="/images/row1.png" alt="" className="w-auto h-40" />
-          <img src="/images/row1.png" alt="" className="w-auto h-40" />
+         <img
+    src="/images/row1.png"
+    className="h-32 md:h-52  min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+         <img
+    src="/images/row1.png"
+    className="h-32 md:h-52  min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+         <img
+    src="/images/row1.png"
+    className="h-32 md:h-52  min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+      
+  
         </motion.div>
-        <motion.div className="flex"
-          animate={{ x: ["-100%", "0%", "-100%"] }}
+        <motion.div  className="flex gap-0 -translate-x-[100%] w-full "
+          animate={{ x: ["0%","100%","0%"] }}
           transition={{
             duration: 20,
             repeat: Infinity,
             ease: "linear"
           }}
         >
-          <img src="/images/row2.png" className="w-auto h-40" alt="" />
-          <img src="/images/row2.png" className="w-auto h-40" alt="" />
-          <img src="/images/row2.png" className="w-auto h-40" alt="" />
-          <img src="/images/row2.png" className="w-auto h-40" alt="" />
+   
+            <img
+    src="/images/row2.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+  <img
+    src="/images/row2.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+  <img
+    src="/images/row2.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+
+      
         </motion.div>
-        <motion.div
-          className="flex"
+         <motion.div
+          className="flex gap-0 w-full "
           animate={{ x: ["0%", "-100%", "0%"] }} // move left continuously
           transition={{
             duration: 20,
@@ -46,11 +72,21 @@ const Clientele = () => {
             ease: "linear",
           }}
         >
-          {/* Duplicate images to loop seamlessly */}
-          <img src="/images/row3.png" alt="" className="w-auto h-40" />
-          <img src="/images/row3.png" alt="" className="w-auto h-40" />
-          <img src="/images/row3.png" alt="" className="w-auto h-40" />
-          <img src="/images/row3.png" alt="" className="w-auto h-40" />
+         <img
+    src="/images/row3.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+  <img
+    src="/images/row3.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
+  <img
+    src="/images/row3.png"
+    className="h-32 md:h-52 w-full min-w-fit flex-shrink-0 object-cover"
+    alt=""
+  />
         </motion.div>
         <div className="absolute w-full h-[100%] inset-0 bg-radial from-[rgb(0,0,0,0)] to-[#ffffff] to 100% pointer-events-none"></div>
       </div>
